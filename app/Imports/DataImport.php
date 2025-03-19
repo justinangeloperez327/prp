4da93cb4f3dbd0df8af9b57d693cfb454def8972
justin <justin@redhq.com.au>
2025-03-19 11:04:42 +0400

    Uncomment data import sheets for Product Categories, Products, Product Items, Customers, and Contacts

--- app/Imports/DataImport.php
+++ app/Imports/DataImport.php
@@ -9,11 +9,11 @@ class DataImport implements WithMultipleSheets
     public function sheets(): array
     {
         return [
-            // 0 => new ProductCategoriesSheetImport,
-            // 1 => new ProductsSheetImport,
-            // 2 => new ProductItemsSheetImport,
-            // 3 => new CustomersSheetImport,
-            // 4 => new ContactsSheetImport,
+            0 => new ProductCategoriesSheetImport,
+            1 => new ProductsSheetImport,
+            2 => new ProductItemsSheetImport,
+            3 => new CustomersSheetImport,
+            4 => new ContactsSheetImport,
             5 => new DiscountsSheetImport
         ];
     }
