6d8a462271ef5a745057d7c7f177e2ae51ebf5b0
justin <justin@redhq.com.au>
2025-04-02 07:50:16 +0400

    Remove bulk delete action from ProductItemResource table configuration

--- app/Filament/Admin/Resources/ProductItemResource.php
+++ app/Filament/Admin/Resources/ProductItemResource.php
@@ -153,9 +153,9 @@ class ProductItemResource extends Resource
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                // Tables\Actions\BulkActionGroup::make([
+                //     Tables\Actions\DeleteBulkAction::make(),
+                // ]),
             ]);
     }
 
